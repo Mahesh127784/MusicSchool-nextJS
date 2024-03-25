87dda58ac5db7349eba958f6add8aac73cbcc59d
MaheshNaik <mn127184@gmail.com>
2024-03-25 10:11:18 +0530

    coures page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CoursesFeatured from "@/components/CoursesFeatured";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <CoursesFeatured />
     </main>
   );
 }
